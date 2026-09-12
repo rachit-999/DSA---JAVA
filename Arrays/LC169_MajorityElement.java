5b234c3a75dedb784cb71e18b8e6ce91d03ffaf1
Rachit <rachitraj296@gmail.com>
2026-09-12 13:15:54 +0530

    Add Solution for LC 169 Majority Element

---
+++ Arrays/LC169_MajorityElement.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+
+        for(int num : nums) {
+            if(map.containsKey(num)) {
+                map.put(num,map.get(num) + 1);
+            }
+            else {
+                map.put(num,1);
+            }
+        }
+
+        for(int num : map.keySet()) {
+            if(map.get(num) > nums.length / 2) {
+                return num;
+            }
+        }
+        return -1;
+    }
+
+
+}
